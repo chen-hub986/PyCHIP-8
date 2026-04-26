62bbee89fe640b93a1cf0e65ecd626f2b38cfd21
Chen Bai Wei <yee980612@gmail.com>
2026-04-27 01:04:57 +0800

    refactor: simplify file path handling in play.py

--- play.py
+++ play.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from pathlib import Path
+
 
 from core.CPU import Chip8cpu
 from core.Display import Display
@@ -8,11 +8,9 @@ from core.input import get_key_state
 
 
 def main() -> None:
-    base_dir = Path(__file__).resolve().parent
-
     pygame.init()
     pygame.mixer.init()
-    beep_sound = pygame.mixer.Sound(str(base_dir / "beep.mp3"))
+    beep_sound = pygame.mixer.Sound(str("beep.mp3"))
     beep_sound.set_volume(0.1)
     pygame.display.set_caption("PyChip8")
     display = Display(scale=10)
@@ -22,7 +20,7 @@ def main() -> None:
     time_event = pygame.USEREVENT + 1
     pygame.time.set_timer(time_event, 17)
 
-    cpu.load_rom(str(base_dir / "roms" / "Pong.ch8"))
+    cpu.load_rom(str("PONG"))
 
     running = True
 
@@ -44,7 +42,7 @@ def main() -> None:
             beep_sound.play()
             print("BEEP!")
 
-        clock.tick(60)
+        clock.tick(50)
 
     display.quit()
     sys.exit()
